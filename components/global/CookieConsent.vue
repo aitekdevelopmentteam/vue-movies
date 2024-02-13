b47774d93d25eeda738d132423d6a59ee0800aaa
Emil Alvaro Serrano Danguilan <158530478+emilalvaro2024@users.noreply.github.com>
2024-02-13 19:45:48 +0800

    Update CookieConsent.vue

--- components/global/CookieConsent.vue
+++ components/global/CookieConsent.vue
@@ -2,7 +2,7 @@
   <div
     v-if="isOpen"
     class="alert alert--alt">
-    <p>We use cookies and other tracking technologies to improve your browsing experience on our website. By using our website, you consent to our use of cookies and other tracking technologies. <a target="_blank" href="https://jason.codes/cookie-policy" rel="noopener">Find out more</a>.</p>
+    <p>Our website utilizes cookies and various tracking technologies to enhance your browsing experience. By accessing and navigating through our site, you agree to our employment of cookies and similar tracking mechanisms.. <a target="_blank" href="https://jason.codes/cookie-policy" rel="noopener">Find out more</a>.</p>
 
     <div>
       <button
